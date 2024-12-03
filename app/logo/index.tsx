3956b41d459973406401d82fcaefff88c32d0db5
YAMI <38581557+ymssx@users.noreply.github.com>
2024-12-03 13:25:43 +0800

    Update index.tsx

--- app/logo/index.tsx
+++ app/logo/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default () => {
+export default function Logo() {
   return (
     <div className="flex gap-2">
       <Image src="/avatar.jpeg" width={64} height={64} alt="YAMI" />
